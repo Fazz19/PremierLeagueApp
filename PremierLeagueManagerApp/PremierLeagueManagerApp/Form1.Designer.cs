2ad8c89bdbf15b91021a70f72283f2f9031256b9
Fazz19 <amicifarid03@gmail.com>
2024-05-25 17:30:02 +0100

    New Project
    Premier League Application

---
+++ PremierLeagueManagerApp/PremierLeagueManagerApp/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace PremierLeagueManagerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn1Clicks = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Btn1Clicks
+            // 
+            this.Btn1Clicks.Location = new System.Drawing.Point(453, 316);
+            this.Btn1Clicks.Name = "Btn1Clicks";
+            this.Btn1Clicks.Size = new System.Drawing.Size(120, 60);
+            this.Btn1Clicks.TabIndex = 0;
+            this.Btn1Clicks.Text = "Button";
+            this.Btn1Clicks.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Btn1Clicks);
+            this.Name = "Form1";
+            this.Text = "Premier League App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn1Clicks;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ PremierLeagueManagerApp/PremierLeagueManagerApp/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PremierLeagueManagerApp
+{
+    public partial class Form1 : Form
+    {
+        private Label lblTeamInfo;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Initialize lblTeamInfo here
+            lblTeamInfo = new Label
+            {
+                Location = new System.Drawing.Point(10, 10), // Example position
+                Size = new System.Drawing.Size(200, 30),     // Example size
+                Text = "Loading..."
+            };
+            this.Controls.Add(lblTeamInfo); // Add the label to the form
+
+            // Assign the event handler correctly
+            this.Btn1Clicks.Click += new System.EventHandler(this.Btn1Click);
+        }
+
+        public async Task<string> GetTeamInfoAsync()
+        {
+            var client = new HttpClient();
+            var requestUrl = "https://fantasy.premierleague.com/api/bootstrap-static/";
+            var response = await client.GetAsync(requestUrl);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        private async void Btn1Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string teamInfo = await GetTeamInfoAsync();
+                lblTeamInfo.Text = teamInfo; // Display the fetched data in lblTeamInfo
+            }
+            catch (Exception ex)
+            {
+                lblTeamInfo.Text = "Error fetching data: " + ex.Message;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
